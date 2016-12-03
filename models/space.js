110bcf69c85c3a2c806ab609db2a3914b95a084e
Herman Ho <contact.hermanho@gmail.com>
2016-12-02 23:27:46 -0500

    fixed spacing

--- models/space.js
+++ models/space.js
@@ -24,7 +24,7 @@ module.exports = function(sequelize, DataTypes) {
     },
     state: {
       type: DataTypes.STRING,
-      allowNull:false,
+      allowNull: false,
       validate: {
         notEmpty: true,
         len: {
